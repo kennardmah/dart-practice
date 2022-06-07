b4906be33ae43f9501f820fb37349f3a9ee1aaff
Ken <73114932+kennardmah@users.noreply.github.com>
2022-06-07 23:18:15 +0100

    upload

---
+++ hello-function2.dart
@@ -0,0 +1,9 @@
+class FlutterDev {
+  FlutterDev(String name) {
+    print('I am $name a Flutter Developer');
+  }
+}
+
+void main() {
+  var typeOfDeveloper = new FlutterDev('Kennard');
+}
---
+++ hello-maps.dart
@@ -0,0 +1,6 @@
+void main() {
+  Map starWars = {"title": "Star Wars", "years": 1997};
+  print(starWars);
+  print(starWars['title']);
+  print(starWars['years']);
+}
